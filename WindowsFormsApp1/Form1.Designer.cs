895e6abea094ebe72bd362e4e7323dee64ca9d0c
AntonBuketovEST <buketov.95@gmail.com>
2022-09-15 13:02:20 +0300

    dice roll + show summ + exit

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Roll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(238, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(241, 145);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 63);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(395, 346);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 49);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(414, 145);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(102, 63);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,11 +10,116 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
-    {
+    public partial class Form1 : Form{
+
+        Random dice = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int randomNum1 = dice.Next(1,7);
+            int randomNum2 = dice.Next(1, 7);
+
+            switch (randomNum1) {
+                case 1:
+                    pictureBox1.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice1.png";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 2:
+                    pictureBox1.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice2.png";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    break;
+                case 3:
+                    pictureBox1.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice3.png";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 4:
+                    pictureBox1.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice4.png";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    break;
+                case 5:
+                    pictureBox1.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice5.png";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 6:
+                    pictureBox1.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice6.png";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                default:
+                    pictureBox1.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice1.png";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+            }
+
+
+
+            switch (randomNum2)
+            {
+                case 1:
+
+                    pictureBox3.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice1.png";
+                    pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 2:
+
+                    pictureBox3.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice2.png";
+                    pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    break;
+                case 3:
+
+                    pictureBox3.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice3.png";
+                    pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 4:
+
+                    pictureBox3.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice4.png";
+                    pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    break;
+                case 5:
+                    pictureBox3.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice5.png";
+                    pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 6:
+
+                    pictureBox3.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice6.png";
+                    pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                default:
+                    pictureBox3.ImageLocation = @"C:\Users\buket\source\repos\WindowsFormsApp1\WindowsFormsApp1\img\dice1.png";
+                    pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+                    break;
+
+            }
+            string summ = Convert.ToString(randomNum1 + randomNum2);
+
+            label1.Text = "numbers fell out " + randomNum1 + " and " + randomNum2 + " total amount is: " + summ;
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
